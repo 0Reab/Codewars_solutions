b435e2f5200b1925576c57a1f4181376d5dad214
0Reab <mihajlofrancic@gmail.com>
2025-01-28 19:00:20 +0100

    Refactored using match case. Closes #8

--- 6_kyu/Who_likes_it.py
+++ 6_kyu/Who_likes_it.py
@@ -22,13 +22,9 @@ Note: For 4 or more names, the number in `"and 2 others"` simply increases.
 '''
 
 def likes(names):
-    if names == []:
-        return "no one likes this"
-    if len(names) == 1:
-        return f"{names[0]} likes this"
-    if len(names) == 2:
-        return f"{names[0]} and {names[1]} like this"
-    if len(names) == 3:
-        return f"{names[0]}, {names[1]} and {names[2]} like this"
-    else:
-        return f"{names[0]}, {names[1]} and {len(names)-2} others like this"
+    match len(names):
+        case 0: return "no one likes this"
+        case 1: return f"{names[0]} likes this"
+        case 2: return f"{names[0]} and {names[1]} like this"
+        case 3: return f"{names[0]}, {names[1]} and {names[2]} like this"
+        case _: return f"{names[0]}, {names[1]} and {len(names)-2} others like this"
